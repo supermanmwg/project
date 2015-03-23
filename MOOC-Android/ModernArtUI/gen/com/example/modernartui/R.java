40144505a7b1a91cb61c065ac596c01cb442f828
supermanmwg <554607289@qq.com>
2015-03-23 23:27:38 +0800

    add count down animation

--- MOOC-Android/ModernArtUI/gen/com/example/modernartui/R.java
+++ MOOC-Android/ModernArtUI/gen/com/example/modernartui/R.java
@@ -14,21 +14,36 @@ public final class R {
         public static final int haha=0x7f020000;
         public static final int haha_part=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int p3=0x7f020003;
+        public static final int p4=0x7f020004;
+        public static final int p5=0x7f020005;
+        public static final int p6=0x7f020006;
+        public static final int p7=0x7f020007;
+        public static final int p8=0x7f020008;
+        public static final int view_animation=0x7f020009;
     }
     public static final class id {
-        public static final int blue=0x7f060005;
-        public static final int easter_egg=0x7f060002;
-        public static final int green=0x7f060003;
-        public static final int red=0x7f060000;
-        public static final int seek_bar=0x7f060006;
-        public static final int white=0x7f060004;
-        public static final int yellow=0x7f060001;
+        public static final int blue=0x7f070005;
+        public static final int easter_egg=0x7f070002;
+        public static final int green=0x7f070003;
+        public static final int more_information=0x7f070008;
+        public static final int my_nephew=0x7f070007;
+        public static final int red=0x7f070000;
+        public static final int seek_bar=0x7f070006;
+        public static final int white=0x7f070004;
+        public static final int yellow=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int nephew=0x7f030001;
+        public static final int webview=0x7f030002;
+    }
+    public static final class menu {
+        public static final int top_menu=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int more_information=0x7f040001;
     }
     public static final class style {
         /** 
--- MOOC-Android/ModernArtUI/src/com/example/modernartui/MainActivity.java
+++ MOOC-Android/ModernArtUI/src/com/example/modernartui/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.modernartui;
 
-import android.R.integer;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.transition.Visibility;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener{
-	
+public class MainActivity extends Activity implements OnClickListener {
+
 	private TextView redView;
 	private TextView greenView;
 	private TextView whiteView;
@@ -21,20 +34,29 @@ public class MainActivity extends Activity implements OnClickListener{
 	private TextView yellowView;
 	private ImageView easterEgg;
 	private SeekBar seekBar;
-	
+
 	private int red = 0xFFCC0000;
 	private int white = 0xFFFFFFFF;
 	private int blue = 0xFF0000FF;
 	private int green = 0xFF99CC00;
 	private int yellow = 0xFF3E2723;
-
+	
+	//Dialog
+	private DialogFragment mDialog;
+	
+	//visit webView
+	private static WebView webView;
+	
+	//easter egg animation
+	private AnimationDrawable mAnim=null;
+	private ImageView imageView;
 	@Override
 	protected void onCreate(Bundle savedState) {
 		super.onCreate(savedState);
 		setContentView(R.layout.main);
-		
+
 		initTextView();
-		seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
+		seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress,
@@ -44,21 +66,87 @@ public class MainActivity extends Activity implements OnClickListener{
 
 			@Override
 			public void onStartTrackingTouch(SeekBar seekBar) {
-				
+
 			}
 
 			@Override
 			public void onStopTrackingTouch(SeekBar seekBar) {
-				
+
 			}
-			
+
 		});
 
+	}
+
+	// Create Options Menu
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.top_menu, menu);
+		return true;
+	}
+
+	// Process clicks on Options Menu items
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.more_information:
+			mDialog = MoreInfoFragment.newInstance();
+			mDialog.show(getFragmentManager(), "MoreInfo");
+			return true;
+		default:
+			return false;
+		}
+	}
+
+	// create the more information dialog
+	public static class MoreInfoFragment extends DialogFragment {
+
+		public static MoreInfoFragment newInstance() {
+			return new MoreInfoFragment();
+		}
+
+		// Build more information dialog using AlertDialog.Builder
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			return new AlertDialog.Builder(getActivity())
+					.setMessage(
+							"Inspired by the works of artists such as Piet Mondrian and Ben Nicholson.\n\nClick below to learn more!")
+					.setCancelable(false)
+					.setPositiveButton("Visit MOMA", 
+							new DialogInterface.OnClickListener() {
+						
+								@SuppressLint("SetJavaScriptEnabled")
+								@Override
+								public void onClick(DialogInterface dialog, int which) {
+									webView =(WebView) getActivity().getLayoutInflater().inflate(R.layout.webview, null);
+									getActivity().setContentView(webView);	
+									webView.setWebViewClient(new HelloWebViewClient());	
+									webView.getSettings().setJavaScriptEnabled(true);
+									webView.loadUrl("www.moma.org/");
+															
+								}
+								
+								class HelloWebViewClient extends WebViewClient {
+									private static final String TAG = "HelloWebViewClient";;
+
+									// Give application a chance to catch additional URL loading requests
+									@Override
+									public boolean shouldOverrideUrlLoading(WebView view, String url) {
+										Log.i(TAG, "About to load:" + url);
+										view.loadUrl(url);
+										return true;
+									}
+								}
+							})
+					.setNegativeButton("Not Now", null)
+					.create();
+		}
 	}
 	
 	@Override
 	public void onClick(View v) {
-		switch(v.getId()) {
+		switch (v.getId()) {
 		case R.id.red:
 			redView.setVisibility(View.INVISIBLE);
 			break;
@@ -72,7 +160,13 @@ public class MainActivity extends Activity implements OnClickListener{
 		case R.id.easter_egg:
 			yellowView.setVisibility(View.INVISIBLE);
 			easterEgg.setBackgroundResource(R.drawable.haha_part);
-			Toast.makeText(getApplicationContext(), "haha", Toast.LENGTH_SHORT).show();
+			// toastCustom();
+			setContentView(R.layout.nephew);
+			imageView = (ImageView)findViewById(R.id.my_nephew);
+			imageView.setBackgroundResource(R.drawable.view_animation);
+			mAnim = (AnimationDrawable) imageView.getBackground();
+			mAnim.start();
+
 			break;
 		case R.id.green:
 			greenView.setVisibility(View.INVISIBLE);
@@ -80,36 +174,65 @@ public class MainActivity extends Activity implements OnClickListener{
 		case R.id.blue:
 			blueView.setVisibility(View.INVISIBLE);
 			break;
-			
+
 		default:
 			break;
 		}
 	}
 	
+	@Override
+	protected void onPause() {
+ 		super.onPause();
+		if (mAnim!=null&&mAnim.isRunning()) {
+			mAnim.stop();
+		}
+	}
+
+
 	private void initTextView() {
-		redView = (TextView)findViewById(R.id.red);
-		whiteView = (TextView)findViewById(R.id.white);
-		yellowView = (TextView)findViewById(R.id.yellow);
-		greenView = (TextView)findViewById(R.id.green);
-		blueView = (TextView)findViewById(R.id.blue);
-		easterEgg = (ImageView)findViewById(R.id.easter_egg);
+		redView = (TextView) findViewById(R.id.red);
+		whiteView = (TextView) findViewById(R.id.white);
+		yellowView = (TextView) findViewById(R.id.yellow);
+		greenView = (TextView) findViewById(R.id.green);
+		blueView = (TextView) findViewById(R.id.blue);
+		easterEgg = (ImageView) findViewById(R.id.easter_egg);
 		redView.setOnClickListener(this);
 		whiteView.setOnClickListener(this);
 		yellowView.setOnClickListener(this);
 		greenView.setOnClickListener(this);
 		blueView.setOnClickListener(this);
 		easterEgg.setOnClickListener(this);
-		
-		seekBar = (SeekBar)findViewById(R.id.seek_bar);
-	}
-	
-	private void changeColor(int p) {
-		int progress = p*5;
-		redView.setBackgroundColor(red+progress);
-		whiteView.setBackgroundColor(white);
-		yellowView.setBackgroundColor(yellow+progress);
-		greenView.setBackgroundColor(green+progress);
-		blueView.setBackgroundColor(blue+progress);
+
+		seekBar = (SeekBar) findViewById(R.id.seek_bar);
 	}
 
+	private void changeColor(int p) {
+		int progress = p * 5;
+		redView.setBackgroundColor(red + progress);
+		whiteView.setBackgroundColor(white);
+		yellowView.setBackgroundColor(yellow + progress);
+		greenView.setBackgroundColor(green + progress);
+		blueView.setBackgroundColor(blue + progress);
+	}
+
+	private void toastCustom() {
+		Toast toast = new Toast(getApplicationContext());
+		toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+		toast.setDuration(Toast.LENGTH_LONG);
+		View view = getLayoutInflater().inflate(R.layout.main, null);
+		redView = (TextView) view.findViewById(R.id.red);
+		whiteView = (TextView) view.findViewById(R.id.white);
+		yellowView = (TextView) view.findViewById(R.id.yellow);
+		greenView = (TextView) view.findViewById(R.id.green);
+		blueView = (TextView) view.findViewById(R.id.blue);
+		easterEgg = (ImageView) view.findViewById(R.id.easter_egg);
+		redView.setOnClickListener(this);
+		whiteView.setOnClickListener(this);
+		yellowView.setOnClickListener(this);
+		greenView.setOnClickListener(this);
+		blueView.setOnClickListener(this);
+		easterEgg.setOnClickListener(this);
+		toast.setView(view);
+		toast.show();
+	}
 }
